f63cb16fcc25f59fd582862cb13f14bda93282e1
peter <psvehla@hotmail.com>
2020-01-17 08:51:23 +1100

    Initial commit.

---
+++ src/main/java/au/com/redbarn/animal/sentence/SentenceApplication.java
@@ -0,0 +1,13 @@
+package au.com.redbarn.animal.sentence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/au/com/redbarn/animal/sentence/SentenceApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.redbarn.animal.sentence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
